07a598e0bb53705dcc61a8bfa67cdeb9356a26b5
Pierre-Jean Quilleré <pj.quillere@gmail.com>
2016-06-29 21:56:01 +0900

    Reorganize files and set up the framework

---
+++ Source/AdvancedCollectionView.h
@@ -0,0 +1,51 @@
+//
+//  AdvancedCollectionView.h
+//  AdvancedCollectionView
+//
+//  Created by Pierre-Jean Quillere on 27/06/2016.
+//  Copyright © 2016 Pierre-Jean Quilleré. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdvancedCollectionView.
+FOUNDATION_EXPORT double AdvancedCollectionViewVersionNumber;
+
+//! Project version string for AdvancedCollectionView.
+FOUNDATION_EXPORT const unsigned char AdvancedCollectionViewVersionString[];
+
+
+// Categories
+#import <AdvancedCollectionView/AAPLDataSource+Headers.h>
+
+// Data Sources
+#import <AdvancedCollectionView/AAPLDataSourceMetrics.h>
+#import <AdvancedCollectionView/AAPLDataSource.h>
+#import <AdvancedCollectionView/AAPLBasicDataSource.h>
+#import <AdvancedCollectionView/AAPLComposedDataSource.h>
+#import <AdvancedCollectionView/AAPLKeyValueDataSource.h>
+#import <AdvancedCollectionView/AAPLSegmentedDataSource.h>
+#import <AdvancedCollectionView/AAPLTextValueDataSource.h>
+
+// Layouts
+#import <AdvancedCollectionView/AAPLLayoutMetrics.h>
+
+// View Controllers
+#import <AdvancedCollectionView/AAPLCollectionViewController.h>
+
+// Views
+#import <AdvancedCollectionView/AAPLCollectionViewCell.h>
+#import <AdvancedCollectionView/AAPLBasicCell.h>
+#import <AdvancedCollectionView/AAPLKeyValueCell.h>
+#import <AdvancedCollectionView/AAPLPinnableHeaderView.h>
+#import <AdvancedCollectionView/AAPLSectionHeaderView.h>
+#import <AdvancedCollectionView/AAPLSegmentedHeaderView.h>
+#import <AdvancedCollectionView/AAPLTextValueCell.h>
+
+// Others
+#import <AdvancedCollectionView/AAPLAction.h>
+#import <AdvancedCollectionView/AAPLContentLoading.h>
+#import <AdvancedCollectionView/AAPLStateMachine.h>
+#import <AdvancedCollectionView/AAPLTheme.h>
+
+
---
+++ Tests/AdvancedCollectionViewTests.m
@@ -0,0 +1,39 @@
+//
+//  AdvancedCollectionViewTests.m
+//  AdvancedCollectionViewTests
+//
+//  Created by Pierre-Jean Quillere on 27/06/2016.
+//  Copyright © 2016 Pierre-Jean Quilleré. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AdvancedCollectionViewTests : XCTestCase
+
+@end
+
+@implementation AdvancedCollectionViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
